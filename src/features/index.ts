b497c4da5a0bd62ff523149dc898eaf3a4ca4597
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-05-04 14:41:45 -0400

    chore: pass function

--- src/features/index.ts
+++ src/features/index.ts
@@ -5,15 +5,15 @@ import { log } from "utils/log";
 export const onAjaxedPagesRaw = async (callback: () => void): Promise<void> => {
     await onDomReady;
     document.addEventListener("pjax:end", callback);
-    callback();
+    return callback();
 };
 
 export const featureSet = new Set<Feature>();
 export const initAll = () =>
-    featureSet.forEach(async ({ id, init, include, load }) => {
+    featureSet.forEach(async ({ id, include, init, load }) => {
         if (include()) {
             try {
-                await load(init());
+                await load(init);
                 log("✅", id);
             } catch (error) {
                 log("❌", id);
