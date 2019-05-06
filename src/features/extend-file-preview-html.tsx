382f76ed02ee1f1519f00fda5c153c9154034361
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-05-06 03:29:42 -0400

    feat: preview html content

--- src/features/extend-file-preview-html.tsx
+++ src/features/extend-file-preview-html.tsx
@@ -1,13 +1,19 @@
+import { PositionProperty } from "csstype";
 import React from "dom-chef";
 import { featureSet, onAjaxedPagesRaw } from "features";
 import select from "select-dom";
+import * as api from "utils/api";
 import { getFileType } from "utils/file-type";
 import { log } from "utils/log";
 import { observeEl } from "utils/mutation-observer";
 import { isPRFiles, isSingleFile } from "utils/page-detect";
 import { selectOrThrow } from "utils/select-or-throw";
+import { getCleanPathname } from "utils/url-path";
 
 const htmlTypes: Set<string> = new Set(["html", "xhtml"]);
+
+const pathToBlob = (path: string) => `https://raw.githubusercontent.com${path}`;
+
 const asNode = (element: JSX.Element): Node => (element as unknown) as Node;
 const viewerButtonToggleGroup = ({ isPR }: { isPR: boolean }) => (
     <span className={`BtnGroup ghprv ${isPR ? "mt-n1" : ""}`}>
@@ -60,11 +66,37 @@ const viewerButtonToggleGroup = ({ isPR }: { isPR: boolean }) => (
     </span>
 );
 
+const frameStyle = {
+    background: "white",
+    border: "none",
+    height: "100%",
+    left: 0,
+    padding: 0,
+    position: "absolute" as PositionProperty,
+    top: 0,
+    width: "100%",
+};
+const frameElement = (props: { src: string; srcDoc: string }) => (
+    <iframe style={frameStyle} className="ghprv" {...props} />
+);
+
 const addButtonsToFileHeaderActions = (actionsElem: HTMLElement): void => {
     if (select.exists(".BtnGroup.ghprv", actionsElem)) return;
     actionsElem.insertBefore(
         asNode(viewerButtonToggleGroup({ isPR: isPRFiles() })),
         actionsElem.firstChild,
+    );
+};
+
+const addFrameToFileBody = (
+    bodyElem: HTMLElement,
+    frameURL: string,
+    frameHTML: string,
+): void => {
+    if (select.exists("iframe.ghprv", bodyElem)) return;
+    bodyElem.style.position = "relative";
+    bodyElem.appendChild(
+        asNode(frameElement({ src: frameURL, srcDoc: frameHTML })),
     );
 };
 
@@ -89,13 +121,20 @@ const initPRFiles = async (): Promise<void> => {
 };
 
 const initSingleFile = async (): Promise<void> => {
-    const fileType = getFileType(window.location.pathname);
+    const filePath = window.location.pathname.replace("/blob/", "/");
+    const fileType = getFileType(filePath);
     const fileHeaderElem: HTMLElement = selectOrThrow(
         ".Box.mt-3>.Box-header.py-2",
     );
-    if (htmlTypes.has(fileType)) {
-        addButtonsToFileHeaderActions(selectOrThrow(".py-1", fileHeaderElem));
-    }
+    if (!htmlTypes.has(fileType)) return;
+    addButtonsToFileHeaderActions(selectOrThrow(".d-flex", fileHeaderElem));
+    const fileURL = pathToBlob(filePath);
+    const fileHTML = await fetch(fileURL).then(r => r.text());
+    addFrameToFileBody(
+        selectOrThrow(".Box.mt-3>.Box-body.blob-wrapper"),
+        fileURL,
+        fileHTML,
+    );
 };
 
 const init = async (): Promise<boolean> => {
