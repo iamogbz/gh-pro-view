f98b3139620f5e41d4bf1842e90973b8bcaca1da
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-05-06 05:04:21 -0400

    refactor: disabled html preview in pr

--- src/features/extend-file-preview-html.tsx
+++ src/features/extend-file-preview-html.tsx
@@ -64,57 +64,70 @@ const viewerButtonToggleGroup = ({
 }: {
     frameElem?: HTMLElement;
     isPR: boolean;
-}) => (
-    <span className={`BtnGroup ${featureClass} ${isPR ? "mt-n1" : ""}`}>
-        <button
-            className={`btn btn-sm BtnGroup-item tooltipped tooltipped-${
-                isPR ? "w" : "n"
-            } source ${isPR ? "js-source" : ""} selected`}
-            aria-current="true"
-            aria-label={`Display the source ${isPR ? "diff" : "blob"}`}
-            onClick={showSource(frameElem)}
-            data-disable-with=""
-        >
-            <svg
-                className="octicon octicon-code"
-                viewBox="0 0 14 16"
-                version="1.1"
-                width="14"
-                height="16"
-                aria-hidden="true"
+}) => {
+    const disabled = frameElem ? false : true;
+    const disabledTooltip = "HTML preview disabled";
+    return (
+        <span className={`BtnGroup ${featureClass} ${isPR ? "mt-n1" : ""}`}>
+            <button
+                className={`btn btn-sm BtnGroup-item tooltipped tooltipped-${
+                    isPR ? "w" : "n"
+                } source ${isPR ? "js-source" : ""} selected`}
+                disabled={disabled}
+                aria-current="true"
+                aria-label={
+                    disabled
+                        ? disabledTooltip
+                        : `Display the source ${isPR ? "diff" : "blob"}`
+                }
+                onClick={disabled ? null : showSource(frameElem)}
+                data-disable-with=""
             >
-                <path
-                    fill-rule="evenodd"
-                    d="M9.5 3L8 4.5 11.5 8 8 11.5 9.5 13 14 8 9.5 3zm-5 0L0 8l4.5 5L6 11.5 2.5 8 6 4.5 4.5 3z"
-                />
-            </svg>
-        </button>
-        <button
-            className={`btn btn-sm BtnGroup-item tooltipped tooltipped-${
-                isPR ? "w" : "n"
-            } rendered ${isPR ? "js-rendered" : ""}`}
-            disabled={frameElem ? false : true}
-            aria-label={`Display the ${isPR ? "rich diff" : "rendered blob"}`}
-            onClick={showRendered(frameElem)}
-            data-disable-with=""
-        >
-            <svg
-                className="octicon octicon-file"
-                viewBox="0 0 12 16"
-                version="1.1"
-                width="12"
-                height="16"
-                aria-hidden="true"
+                <svg
+                    className="octicon octicon-code"
+                    viewBox="0 0 14 16"
+                    version="1.1"
+                    width="14"
+                    height="16"
+                    aria-hidden="true"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M9.5 3L8 4.5 11.5 8 8 11.5 9.5 13 14 8 9.5 3zm-5 0L0 8l4.5 5L6 11.5 2.5 8 6 4.5 4.5 3z"
+                    />
+                </svg>
+            </button>
+            <button
+                className={`btn btn-sm BtnGroup-item tooltipped tooltipped-${
+                    isPR ? "w" : "n"
+                } rendered ${isPR ? "js-rendered" : ""}`}
+                disabled={disabled}
+                aria-label={
+                    disabled
+                        ? disabledTooltip
+                        : `Display the ${isPR ? "rich diff" : "rendered blob"}`
+                }
+                onClick={disabled ? null : showRendered(frameElem)}
+                data-disable-with=""
             >
-                <path
-                    fill-rule="evenodd"
-                    d={`M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45
+                <svg
+                    className="octicon octicon-file"
+                    viewBox="0 0 12 16"
+                    version="1.1"
+                    width="12"
+                    height="16"
+                    aria-hidden="true"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d={`M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45
                     1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z`}
-                />
-            </svg>
-        </button>
-    </span>
-);
+                    />
+                </svg>
+            </button>
+        </span>
+    );
+};
 
 const frameStyle = {
     background: "white",
