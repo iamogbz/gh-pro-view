b8780676c4e0c1f1644fadcc8648c245ecce83c6
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-05-07 08:05:56 -0400

    test: page detect

--- tests/utils/page-detect.test.ts
+++ tests/utils/page-detect.test.ts
@@ -3,24 +3,24 @@ mockProps.extend(jest);
 
 import * as pageDetect from "utils/page-detect";
 
+const basePath = "/iamogbz/gh-pro-view";
+const location = { pathname: basePath } as Location;
+const windowLocationSpy = jest.spyOnProp(location, "pathname");
+jest.spyOn(window as AnyObject, "location", "get").mockImplementation(
+    () => location,
+);
+afterEach(jest.clearAllMocks);
+afterAll(jest.restoreAllMocks);
+
 describe("isPR", () => {
-    const basePath = "/iamogbz/gh-pro-view";
-    const location = { pathname: basePath } as Location;
-    const windowLocationSpy = jest.spyOnProp(location, "pathname");
-    jest.spyOn(window as AnyObject, "location", "get").mockImplementation(
-        () => location,
-    );
-
-    afterAll(jest.restoreAllMocks);
-
     it.each([
         ["/", false],
         ["/compare/test", false],
         ["/pull/15", true],
-        ["/pull/15/files", true],
         ["/pull/15/commits", true],
+        ["/pull/15/files", true],
     ])("detects that '%s' is '%s'", (path, result) => {
-        windowLocationSpy.mockValueOnce(`${basePath}${path}`);
+        windowLocationSpy.mockValue(`${basePath}${path}`);
         expect(pageDetect.isPR()).toBe(result);
     });
 
@@ -29,11 +29,57 @@ describe("isPR", () => {
             ["/", false],
             ["/compare/test", false],
             ["/pull/15", false],
-            ["/pull/15/files", true],
             ["/pull/15/commits", false],
+            ["/pull/15/files", true],
         ])("detects that '%s' is '%s'", (path, result) => {
-            windowLocationSpy.mockValueOnce(`${basePath}${path}`);
+            windowLocationSpy.mockValue(`${basePath}${path}`);
             expect(pageDetect.isPRFiles()).toBe(result);
+        });
+    });
+
+    describe("commits", () => {
+        it.each([
+            ["/", false],
+            ["/compare/test", false],
+            ["/test/commit/123456789abcdef", false],
+            ["/pull/15/commits", false],
+            ["/pull/15/commits/z3edswas", false],
+            ["/pull/15/commits/123456789abcdef", true],
+        ])("detects that '%s' is '%s'", (path, result) => {
+            windowLocationSpy.mockValue(`${basePath}${path}`);
+            expect(pageDetect.isPRCommit()).toBe(result);
+        });
+    });
+});
+
+describe("single commit", () => {
+    it.each([
+        ["/", false],
+        ["/compare/test", false],
+        ["/test/commit/123456789abcdef", false],
+        ["/pull/15/commits/123456789abcdef", false],
+        ["/commit/z3edswas", false],
+        ["/commit/123456789abcdef", true],
+        ["/commit/12345f", true],
+    ])("detects that '%s' is '%s'", (path, result) => {
+        windowLocationSpy.mockValue(`${basePath}${path}`);
+        expect(pageDetect.isSingleCommit()).toBe(result);
+    });
+
+    describe("with pr", () => {
+        it.each([
+            ["/", false],
+            ["/compare/test", false],
+            ["/test/commit/123456789abcdef", false],
+            ["/pull/15/commits", false],
+            ["/pull/15/commits/z3edswas", false],
+            ["/commit/z3edswas", false],
+            ["/pull/15/commits/123456789abcdef", true],
+            ["/commit/123456789abcdef", true],
+            ["/commit/12345f", true],
+        ])("detects that '%s' is '%s'", (path, result) => {
+            windowLocationSpy.mockValue(`${basePath}${path}`);
+            expect(pageDetect.isCommit()).toBe(result);
         });
     });
 });
