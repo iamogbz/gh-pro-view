c69a14009f83d0344b892ace996f4dfd014e2212
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-07-03 09:00:44 -0400

    fix: update option sync default definitoin

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,10 +1,1 @@
 import "crx-livereload";
-import OptionsSync from "webext-options-sync";
-
-// Define defaults
-new OptionsSync().define({
-    defaults: {
-        personalToken: "",
-    },
-    migrations: [],
-});
